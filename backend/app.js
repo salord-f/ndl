d37a47c06c728d7a809035e0f37e3aee3901c281
Florian Salord <florian.salord@etu.univ-cotedazur.fr>
2019-12-05 20:13:07 +0100

    fixed game

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,6 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
 
 const app = express();
 
@@ -25,7 +24,6 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
