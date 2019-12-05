396fd3c4167c99424bd241d8722d6ae46d677163
Florian Salord <florian.salord@etu.univ-cotedazur.fr>
2019-12-05 20:11:39 +0100

    added basic game

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,10 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const path = require('path');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('404game.html');
 });
 
 module.exports = router;
